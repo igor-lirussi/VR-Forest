053f44cd31da87d24fd55f823506893b4bb0b480
Hatice Şule Erkul <hatice.erkul@boun.edu.tr>
2022-07-01 18:27:16 +0300

    added woodpecker

---
+++ Assets/VRM/Runtime/Format/VRMVersion.cs
@@ -0,0 +1,11 @@
+
+namespace VRM
+{
+    public static partial class VRMVersion
+    {
+        public const int MAJOR = 0;
+        public const int MINOR = 101;
+        public const int PATCH = 0;
+        public const string VERSION = "0.101.0";
+    }
+}
